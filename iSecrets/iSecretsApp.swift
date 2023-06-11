8ef03c5fbebf9049987f7baaf5c2b4ade9e9efd3
dbydu <dbydu@tencent.com>
2023-06-11 17:13:22 +0800

    Initial Commit

---
+++ iSecrets/iSecretsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSecretsApp.swift
+//  iSecrets
+//
+//  Created by dby on 2023/6/11.
+//
+
+import SwiftUI
+
+@main
+struct iSecretsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
