e7a823a2d60020c45f95bfcd5707c7fb83b400bc
dbydu <dbydu@tencent.com>
2023-07-09 16:30:56 +0800

    fix: 调整部分目录

---
+++ iSecrets/Scenes/HomeCoordinator.swift
@@ -0,0 +1,12 @@
+//
+//  HomeCoordinator.swift
+//  iSecrets
+//
+//  Created by dby on 2023/7/9.
+//
+
+import SwiftUI
+
+class HomeCoordinator: ObservableObject {
+    
+}
--- iSecrets/iSecretsApp.swift
+++ iSecrets/iSecretsApp.swift
@@ -13,7 +13,7 @@ struct iSecretsApp: App {
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeContentView()
         }
     }
 }
